a945524bda4844156e451425e21dfb6fd5d583dd
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-11-01 23:31:30 -0700

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def video_url_creator(id_lst):
     videos = []
-    for vid_id in id_lst:
+    for vid_id in id_lst: 
         # We know that embedded YouTube videos always have this format
         video = 'https://youtube.com/embed/' + vid_id
         videos.append(video)
@@ -30,7 +30,7 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
@@ -43,19 +43,52 @@ def playlists_show(playlist_id):
 def playlists_submit():
     """Submit a new playlist."""
     # Grab the video IDs and make a list out of them
-    video_ids = request.form.get('video_ids').split()
+    video_ids = request.form.get('video_ids').split() # ["asdfasdf", "asdfsadf"]
     # call our helper function to create the list of links
-    videos = video_url_creator(video_ids) # ["asdfasdf", "asdfsadf"]
+    videos = video_url_creator(video_ids)  
     playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': videos,
         'video_ids': video_ids,
-        'video_rate': request.form.get('video_rating')
+        'rating': request.form.get('rating')
     }
-    playlists.insert_one(playlist)
+    playlists.insert_one(playlist) #once the playlist db is accessed, you can call the id
+    return redirect(url_for('playlists_show', playlist_id=playlist['_id'])) #look up playlist_id
+    # return render_template('playlists_show.html', playlist=playlist)    
+    # return redirect(url_for('playlists_show'),playlist_id=playlist['_id'])
+
+@app.route('/playlists/<playlist_id>/delete')
+def playlists_delete(playlist_id):
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    # return render_template('playlists_index.html')
     return redirect(url_for('playlists_index'))
 
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)}) 
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids,
+        'rating': request.form.get('rating')
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
